d4ebfd77fa741664dd1e84caa031aa0320031e93
Abdullah <apocoder@gmail.com>
2017-03-27 01:33:52 +0300

    fixed marathon host ip

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ else
   # marathon=${MARATHON_PORT_8080_TCP_ADDR}
 fi
 
-curl -X DELETE -H "Content-Type: application/json" http://${marathon}:8081/v2/apps/app
+curl -X DELETE -H "Content-Type: application/json" http://127.0.0.1:8081/v2/apps/app
 sleep 1
 #curl -X POST -H "Content-Type: application/json" http://localhost:8080/v2/apps -d@app_marathon.json
 
@@ -18,4 +18,4 @@ cp -f app_marathon.json app_marathon.json.tmp
 sed -i "s/latest/${version}/g" app_marathon.json.tmp
 
 
-curl -X POST -H "Content-Type: application/json" http://${marathon}:8081/v2/apps -d@app_marathon.json.tmp
+curl -X POST -H "Content-Type: application/json" http://127.0.0.1:8081/v2/apps -d@app_marathon.json.tmp
